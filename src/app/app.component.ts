9e71f1d8ccc192bb1866b0181d7446c2bedab0ba
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2020-04-08 10:44:58 +0200

    Modifying app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ws-root',
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+      <div>My First Component</div>
+    </div>  
+    `
 })
 export class AppComponent {
-  title = 'MbwsNG';
+  pageTitle: string = "MbWebSiteAngular";
 }
